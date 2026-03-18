f8841c81f073fba0203688903d44a1cd9c42fd60
DanGLVK <switchtegra@gmail.com>
2026-03-17 22:10:39 -0300

    Update atexit.cpp

--- magisk-loader/src/main/jni/src/atexit.cpp
+++ magisk-loader/src/main/jni/src/atexit.cpp
@@ -83,10 +83,7 @@ namespace {
         if (count > 0) {
             size_t total = count;
             for (size_t i = count - 1;; --i) {
-                if (g_array[i].fn == nullptr) {
-                    if (i == 0) break;
-                    continue;
-                }
+                if (g_array[i].fn == nullptr) continue;
 
                 // Clear the entry in the array to avoid calling an entry again
                 // if __cxa_finalize is called recursively.
